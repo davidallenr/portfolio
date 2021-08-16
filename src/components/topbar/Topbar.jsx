1a9a5d4114ae6dce21db4d9a2c5b4bf3e62d6c68
David Russell <davidallenr@gmail.com>
2021-08-16 11:09:15 -0400

    Added css to left topbar
    -Moved links to right wrapper in topbar
    -Added CSS for logo

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -4,13 +4,18 @@ export default function Topbar() {
         <div className = "topbar">
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro">intro</a>
+                    <a href="#intro" className = "logo">David Russell</a>
+                    <div className="itemContainer">
+                        
+                    </div>
+                </div>
+                <div className="right">
+                    <a href="#intro" >Intro</a>
                     <a href="#portfolio">portfolio</a>
                     <a href="#works">works</a>
                     <a href="#testimonials">testimonials</a>
                     <a href="#contact">contact</a>
                 </div>
-                <div className="right"></div>
             </div>
         </div>
     )
