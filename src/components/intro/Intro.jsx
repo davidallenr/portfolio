0f3c18abf8def2671387d0811d29878cf6a658c9
David Russell <davidallenr@gmail.com>
2021-08-17 12:43:05 -0400

    CSS for Intro
    -Added animation to arrow
    -Matched text color to avatar background
    -fixed avatar positioning

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,15 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 
 export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, { showCursor: true, strings: ['Father', 'Developer', 'Student', 'Veteran' ] })
+    }, [])
+
     return (
         <div className = "intro" id = "intro">
             <div className="left">
@@ -13,8 +21,7 @@ export default function Intro() {
                 <div className="wrapper">
                         <h2>Hello! I'm </h2>
                         <h1>David Russell</h1> 
-                        <h3>I'm a passionate <span>Software Developer</span></h3>
-                        <h2><span>Father Developer Student Veteran</span>
+                        <h2>I'm a passionate <span ref = {textRef} />
                             <a href = "#portfolio">
                                 <KeyboardArrowDownIcon/>
                             </a>
