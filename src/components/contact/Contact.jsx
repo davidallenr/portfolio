46de3aa1f5ea66960ee06b5a619b19a9da13e6d7
David Russell <davidallenr@gmail.com>
2021-08-17 14:56:38 -0400

    Began Contact Page
    -Added form and validation
    -Added submit handling
    -Added form CSS to center

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,17 @@
 import "./contact.scss"
 
 export default function Contact() {
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+    }
     return (
         <div className = "contact" id = "contact">
-            
+            <h2>Contact.</h2>
+            <form onSubmit = { handleSubmit }>
+                <input type = "text" placeholder = "Email"/>
+                <textarea placeholder = "Message" ></textarea>
+                <button type = "submit">Submit</button>
+            </form>
         </div>
     )
 }
