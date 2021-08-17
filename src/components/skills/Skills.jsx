d2c6388e271d644cd6e3d83e8dde204ec7cbde6d
David Russell <davidallenr@gmail.com>
2021-08-17 14:55:43 -0400

    Skills formatting and words added

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -2,6 +2,37 @@ import "./skills.scss"
 export default function Skills() {
     return (
         <div className = "skills" id = "skills">
+            <h1>Programming Languages</h1>
+            <p>
+                C++
+                Python
+                Lua
+            </p>
+             <h1>Frontend Development</h1>
+             <p>
+                React
+                React-Native
+                Styled-Components
+                HTML
+                CSS
+                JavaScript
+            </p>
+            <h1>Backend Development</h1>
+            <p>
+                Node
+                PHP
+                GraphQL
+                MYSQL
+                MongoDB
+                SQL
+                REST
+            </p>
+            <h1>Version Control</h1>
+            <p>
+                Github
+                Gitlab
+                Bitbucket
+            </p>
         </div>
     )
 }
