3254f9583a01a4e08d2989699255716dca183243
David Russell <davidallenr@gmail.com>
2021-08-17 11:37:04 -0400

    Beginning Intro CSS
    -No animations added yet
    -left and right classes added to intro
    -img styled
    -Added MUI arrow icon

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,27 @@
 import "./intro.scss"
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 
 export default function Intro() {
     return (
         <div className = "intro" id = "intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/avataaars.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                        <h2>Hello! I'm </h2>
+                        <h1>David Russell</h1> 
+                        <h3>I'm a passionate <span>Software Developer</span></h3>
+                        <h2><span>Father Developer Student Veteran</span>
+                            <a href = "#portfolio">
+                                <KeyboardArrowDownIcon/>
+                            </a>
+                        </h2>
+                        
+                </div>
+            </div>
         </div>
     )
 }
