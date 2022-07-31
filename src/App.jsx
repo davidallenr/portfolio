14c80bf3a10d7e23aad3c48b214d167b16565748
David Russell <davidallenr@gmail.com>
2022-07-31 12:34:10 -0400

    added icons and adjust app/css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss"
 import Form from "./components/Form";
 import { createContext, useState } from "react";
 import ReactSwitch from "react-switch";
+import { BsMoonStarsFill } from 'react-icons/bs';
 
 export const ThemeContext = createContext(null);
 
@@ -11,13 +12,14 @@ function App() {
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
   };
-  
+
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
         <Form />
         <div className="switch">
-          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} uncheckedIcon={<BsMoonStarsFill />}/>
         </div>
       </div>
     </ThemeContext.Provider>
