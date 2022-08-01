6c1c3e54e6e27f3e9ffe4af24cebfe6811537b00
David Russell <davidallenr@gmail.com>
2022-08-01 07:59:50 -0400

    local storage save theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
 import "./app.scss"
 import Form from "./components/Form";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import ReactSwitch from "react-switch";
-import { BsMoonStarsFill } from 'react-icons/bs';
+import { BsMoonStarsFill, BsSunFill } from 'react-icons/bs';
 
 export const ThemeContext = createContext(null);
 
 function App() {
   const [theme, setTheme] = useState("dark");
+  const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
   const toggleTheme = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+    setTheme((curr) => {
+      localStorage.setItem('theme', curr === "light" ? "dark" : "light");
+      return (curr === "light" ? "dark" : "light");
+    });
   };
+  
+  useEffect(()=> {
+    if (currentTheme) {
+      setTheme(currentTheme);
+    }
+  }, [currentTheme])
+
+  
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
         <Form />
         <div className="switch">
-        
-          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} uncheckedIcon={<BsMoonStarsFill />}/>
+          <ReactSwitch onChange={toggleTheme} checked={currentTheme === "dark"} checkedIcon={<BsSunFill />} uncheckedIcon={<BsMoonStarsFill />}/>
         </div>
       </div>
     </ThemeContext.Provider>
