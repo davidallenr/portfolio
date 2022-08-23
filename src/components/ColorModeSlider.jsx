a00f1a3f9bf008d0072899bfd0e45f63e5b79b56
David Russell <davidallenr@gmail.com>
2022-08-23 08:14:37 -0400

    slider working

--- src/components/ColorModeSlider.jsx
+++ src/components/ColorModeSlider.jsx
@@ -4,8 +4,8 @@ const ColorModeSlider = ({toggleTheme, currentTheme}) => {
   return (
     <React.Fragment>
       <div className="slider">
-        <input type="checkbox" id="hide-checkbox" />
-        <label htmlFor="hide-checkbox" onClick={toggleTheme} className="toggle">
+        <input type="checkbox" id="hide-checkbox" defaultChecked={ currentTheme === "light" ? true : false}/>
+        <label htmlFor="hide-checkbox" onClick={toggleTheme} className="toggle" >
           <span className="toggle-button">
             {[...Array(7)].map((v,i) => {
               return <span key={"crater" + i} className={"crater crater-" + (i + 1)} ></span>
